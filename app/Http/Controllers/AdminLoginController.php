38cebfae2d7b27fa5ef96e8cd51f4d3533b79bfa
Tony Nguyen <nguyencuonghut5@gmail.com>
2022-04-07 11:00:42 +0700

    Fix: correct the route to admin home page

--- app/Http/Controllers/AdminLoginController.php
+++ app/Http/Controllers/AdminLoginController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Notification;
 
