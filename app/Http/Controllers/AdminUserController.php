9ee2f18415c4f5c4cf05be7dd833e200baa8c5de
Tony Nguyen <nguyencuonghut5@gmail.com>
2022-04-20 14:04:05 +0700

    Fix: Not allow to create/edit User's password
    - Disable option to create/edit password.
    - Send password to User's email.

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Supplier;
 use App\Models\User;
+use App\Notifications\UserCreated;
 use Datatables;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Notification;
 use RealRashid\SweetAlert\Facades\Alert;
+use Illuminate\Support\Str;
 
 class AdminUserController extends Controller
 {
@@ -49,7 +53,6 @@ class AdminUserController extends Controller
             $rules = [
                 'name' => 'required|max:255',
                 'email' => 'required|email|max:255',
-                'password' => 'required|confirmed|min:6',
                 'supplier_id' => 'required',
             ];
             $messages = [
@@ -59,21 +62,22 @@ class AdminUserController extends Controller
                 'email.required' => 'Bạn phải nhập địa chỉ email.',
                 'email.email' => 'Email sai định dạng.',
                 'email.max' => 'Email dài quá 255 ký tự.',
-                'password.required' => 'Bạn phải nhập mật khẩu.',
-                'password.min' => 'Mật khẩu phải dài ít nhất 6 ký tự.',
-                'password.confirmed' => 'Mật khẩu không khớp.',
                 'supplier_id.required' => 'Bạn phải nhập tên công ty.',
             ];
             $request->validate($rules,$messages);
 
+            $password = Str::random(8);
             $user = new User();
             $user->name = $request->name;
             $user->email = $request->email;
-            $user->password = bcrypt($request->password);
+            $user->password = Hash::make($password);
             $user->supplier_id = $request->supplier_id;
             $user->save();
 
-            Alert::toast('Tạo người dùng mới thành công!', 'success', 'top-right');
+            //Send password to user's email
+            Notification::route('mail' , $user->email)->notify(new UserCreated($user->id, $password));
+
+            Alert::toast('Tạo người dùng mới và gửi mật khẩu tới email họ thành công!', 'success', 'top-right');
             return redirect()->route('admin.users.index');
         }else {
             Alert::toast('Bạn không có quyền tạo tài khoản!', 'error', 'top-right');
@@ -123,7 +127,6 @@ class AdminUserController extends Controller
             $rules = [
                 'name' => 'required|max:255',
                 'email' => 'required|email|max:255',
-                'password' => 'confirmed',
                 'supplier_id' => 'required',
             ];
             $messages = [
@@ -133,7 +136,6 @@ class AdminUserController extends Controller
                 'email.required' => 'Bạn phải nhập địa chỉ email.',
                 'email.email' => 'Email sai định dạng.',
                 'email.max' => 'Email dài quá 255 ký tự.',
-                'password.confirmed' => 'Mật khẩu không khớp.',
                 'supplier_id.required' => 'Bạn phải nhập tên công ty.',
             ];
             $request->validate($rules,$messages);
@@ -141,9 +143,6 @@ class AdminUserController extends Controller
             $user = User::findOrFail($id);
             $user->name = $request->name;
             $user->email = $request->email;
-            if(null != $request->password) {
-                $user->password = bcrypt($request->password);
-            }
             $user->supplier_id = $request->supplier_id;
             $user->save();
             Alert::toast('Cập nhật thông tin thành công!', 'success', 'top-right');
---
+++ app/Notifications/UserCreated.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserCreated extends Notification
+{
+    use Queueable;
+    protected $user_id;
+    protected $password;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($user_id, $password)
+    {
+        $this->user_id = $user_id;
+        $this->password = $password;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = url('/');
+        $user = User::findOrFail($this->user_id);
+        return (new MailMessage)
+                    ->subject('Kích hoạt tài khoản phần mềm Tender Honghafeed ' . $user->name)
+                    ->line('Xin mời kích hoạt tài khoản: ' . $user->name .  ' (' . $user->email . ')')
+                    ->line('Mật khẩu: ' . $this->password)
+                    ->action('Đăng nhập tài khoản của bạn', url($url))
+                    ->line('Xin cảm ơn!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
