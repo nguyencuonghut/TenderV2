8b6af25145cb229a3e4264990d85ec33d3a4d741
Tony Nguyen <nguyencuonghut5@gmail.com>
2022-04-30 23:08:41 +0700

    Fix: correct the year of footer page

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a target="_blank" href="https://honghafeed.com.vn">Công ty cổ phần dinh dưỡng Hồng Hà</a>.</strong>
+    <strong>Copyright &copy; 2021- {{now()->year}} <a target="_blank" href="https://honghafeed.com.vn">Công ty cổ phần dinh dưỡng Hồng Hà</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.1.0
