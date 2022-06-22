c50ee444c4a024583b5b36781388af972fcb899e
Tony Nguyen <nguyencuonghut5@gmail.com>
2022-06-22 14:11:59 +0700

    Fix: correct the icon on User Dashboard

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -82,7 +82,7 @@
             <!-- /.col -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="info-box mb-3">
-                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-truck"></i></span>
+                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-dollar-sign"></i></span>
 
                 <div class="info-box-content">
                     <span class="info-box-text">Chào giá của tôi</span>
