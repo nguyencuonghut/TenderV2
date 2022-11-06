2b1e40640804a2c85d9471d46e3e33e7c743fbd0
Tony Nguyen <nguyencuonghut5@gmail.com>
2022-11-06 15:39:00 +0700

    Fix: display real time clock on footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,6 +2,19 @@
     <strong>Copyright &copy; 2021- {{now()->year}} <a target="_blank" href="https://honghafeed.com.vn">Công ty cổ phần dinh dưỡng Hồng Hà</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        {{now()->day}}/{{now()->month}}/{{now()->year}} - {{now()->hour}}:{{now()->minute}}
+        <p id="time"></p>
     </div>
 </footer>
+
+
+<script src="{{ asset('plugins/jquery/jquery-3.4.0.js') }}"></script>
+<script type="text/javascript">
+var timestamp = '<?=time();?>';
+function updateTime(){
+  $('#time').html(Date(timestamp));
+  timestamp++;
+}
+$(function(){
+  setInterval(updateTime, 1000);
+});
+</script>
