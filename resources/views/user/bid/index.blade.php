621511702dd71926030ce3109a75898c5367593c
Tony Ng <nguyencuonghut55@gmail.com>
2023-10-02 14:47:36 +0700

    Fix: remove duplicate rating
    If duplicate, there is no 1st order, start from 2nd

--- resources/views/user/bid/index.blade.php
+++ resources/views/user/bid/index.blade.php
@@ -86,7 +86,17 @@
                         $my_key = array_search($bid->price, $all_current_bid_prices);
                     @endphp
                     @if($is_rating)
-                    <td>@if(count(array_keys($all_current_bid_prices, $bid->price)) > 1)<i class="fas fa-layer-group" style="color:orange;"></i>@endif {{$my_key + 1}} | {{sizeof($all_current_bid_prices)}} bids</td>
+                    <td>
+                      @if(count(array_keys($all_current_bid_prices, $bid->price)) > 1)
+                        @if(0 == $my_key)
+                          {{$my_key + 2}} | {{sizeof($all_current_bid_prices)}} bids
+                        @else
+                          {{$my_key + 1}} | {{sizeof($all_current_bid_prices)}} bids
+                        @endif
+                      @else
+                        {{$my_key + 1}} | {{sizeof($all_current_bid_prices)}} bids
+                      @endif
+                    </td>
                     @endif
                     @if('Đóng' != $tender->status
                         && Carbon\Carbon::now()->lessThan($tender->tender_end_time))
